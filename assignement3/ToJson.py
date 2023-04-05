45a0948b780682959c22363b68aff768a3e333a3
Mike Wierenga <mk.wierenga@st.hanze.nl>
2023-04-05 11:19:27 +0200

    added plotting

--- assignement3/ToJson.py
+++ assignement3/ToJson.py
@@ -14,7 +14,6 @@ class CsvConverter:
         value = lc.getline(self.file, line).split(',')
         
         if len( value) == 1:
-            # print([])
             return json.dumps("")
         else:
             assert len(keys) == len(value)
@@ -37,9 +36,12 @@ class Reader:
         self.read_line_number +=5       
         return lines
 
+
 class AverageYear:
     def __init__(self, file):
         self.Reader = Reader(file)
+        self.years = []
+        self.averages = []
 
     def calculate_average_year(self):
         dic = {}
@@ -52,7 +54,7 @@ class AverageYear:
                         dic[key].append(int(value))
                 if key in ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']:
                     if "months" not in dic:
-                        print(key)
+          
                         dic["months"] = [float(value)]
                     else:
                         dic["months"].append(float(value))
@@ -63,9 +65,26 @@ class AverageYear:
             dic[key] = sum(value)/ len(value)            
         return dic                
     
+    def average_year_data(self, year_averages):
+        first_year = year_averages['Year'][0]
+        last_year = year_averages['Year'][-1]
+        x_value = f'{first_year} - {last_year}'
+        
+        data = year_averages['months']
+        self.years.append(x_value)
+        self.averages.append(data)
+
+    def plot_year(self):
+        # figure = plt.figure()
+        plt.plot(self.years, self.averages)
+        print(self.years, self.averages)
+        plt.show()
+
+
 class AverageMonth:
     def __init__(self, file):
         self.Reader = Reader(file)
+        self.lines = []
 
     def calculate_average_month(self):
         dic = {}
@@ -91,35 +110,34 @@ class AverageMonth:
         data = y[1:]
        
         # plt.legend(loc="upper left")
-        return  [x,data, label]
-       
+        self.lines.append([x, data, label])
+        
+    def plot_months(self):
+        for x in self.lines:
+            plt.plot(x[0], x[1], label=x[2])
+        plt.legend(loc='upper left')    
+        plt.show()
 
 
-test =   AverageYear('data/dSST.csv')
-"""
+
+
+
 def main():
-    start = input("Enter yes or no to continue:")
- 
-    while start != 'no':
-        print(start)
-        toplot = test.average_month_plot(test.calculate_average_month())
-        plt.plot(toplot[0], toplot[1], label=toplot[2])
-        plt.legend(loc="upper left")
-        start = ''
-        # plt.show()
-        main()
-    plt.show()    
-    return "im done"
+    month =  AverageMonth('data/dSST.csv')
+    year = AverageYear('data/dSST.csv')
+
+    while True:
+        start = input("Enter yes or no to continue:")
+        if start == 'no':
+            break
+        elif start == 'month':
+            month.average_month_plot(month.calculate_average_month())
+            month.plot_months()
+        elif start == 'year':
+            year.average_year_data(year.calculate_average_year())
+            year.plot_year()
+    
 main()
-"""
-print(test.calculate_average_year())
-# plt.show()
-
-
-# test.calculate_average_month()
-# test = Reader('data/dSST.csv')
-
-# print(test.get_line())
 
 
 
