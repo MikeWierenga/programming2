7de42c03655fc07c3ca58f4aea87ed1546e313d9
Mike Wierenga <mk.wierenga@st.hanze.nl>
2023-05-11 17:50:24 +0200

    progress with assignment 2.1

---
+++ assignment2.1/lesson2.1/dataprovider.py
@@ -0,0 +1,26 @@
+import pandas as pd
+class DataProvider:
+    def __init__(self):
+        self.df = pd.read_csv("dSST.csv")    
+    
+    def get_data(self, *kwargs):
+     
+        data = ''
+        if len(kwargs) == 0:
+            data = self.df
+        elif len(kwargs) == 1 and isinstance(kwargs[0], int):
+
+            data = self.df[self.df['Year'] == kwargs[0]]
+        elif len(kwargs) == 1 and isinstance(kwargs[0], list):
+            pass
+            data = self.df.loc[(self.df['Year'] >= kwargs[0][0]) & (self.df['Year'] <= kwargs[0][1])]
+        return data.T.to_json()
+    
+        # if len(kwargs) == 1:
+        #     pass
+        # if kwargs[0] == 'all':
+        #     data = self.df
+        # return data
+
+
+
